a5a0c9ecc2bcb3725b37066eb327028ef10cb88a
arju10 <arjuarju71@gmail.com>
2024-12-17 11:50:00 +0600

    Create Transaction Model

--- bank_management/bank_management/urls.py
+++ bank_management/bank_management/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from core.views import HomeView
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('admin/', admin.site.urls),
