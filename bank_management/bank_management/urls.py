bdc8495456f2ad44ca8d03891426e349d94fb562
arju10 <arjuarju71@gmail.com>
2024-12-17 10:29:40 +0600

    Create HomeView

--- bank_management/bank_management/urls.py
+++ bank_management/bank_management/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls'))
 ]
